d55ce30e92ce6d1d6d2c9b6719dd7c2dbb9d7d08
unknown <30684@fiecalunos.lan>
2019-11-27 19:33:51 -0300

    cassiel1

---
+++ headFooter/footer.php
@@ -0,0 +1,20 @@
+<div style="background-color:rgba(128, 128, 128, 0.4);">
+    <h1 class="text-center">Redes Sociais</h1>
+    <br>
+    <div class="row h-100 justify-content-center" >
+      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-facebook-square fa-3x"></i></div>
+      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-instagram fa-3x"></i></div>
+      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-linkedin-square fa-3x"></i></div>
+      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-twitter-square fa-3x"></i></div>
+      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-youtube-square fa-3x"></i></div>
+      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-twitch fa-3x"></i></div>
+    </div>
+    <br><br>
+  </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,45 +1,5 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Title</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
+ 
+ <?php include_once('headFooter/head.php')  ?>
 
   <img style="width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;"
     src="https://i.ibb.co/dbWLcX5/image-url.jpg" />
@@ -84,27 +44,6 @@
     </div>
   </div>
 
-
-
 <hr>
 
-    <div style="background-color:rgba(128, 128, 128, 0.4);">
-    <h1 class="text-center">Redes Sociais</h1>
-    <br>
-    <div class="row h-100 justify-content-center" >
-      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-facebook-square fa-3x"></i></div>
-      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-instagram fa-3x"></i></div>
-      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-linkedin-square fa-3x"></i></div>
-      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-twitter-square fa-3x"></i></div>
-      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-youtube-square fa-3x"></i></div>
-      <div class="col col-md-1 d-flex justify-content-md-center"><i class="zoom fa fa-twitch fa-3x"></i></div>
-    </div>
-    <br><br>
-  </div>
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+<?php include_once('./headFooter/footer.php') ?>
