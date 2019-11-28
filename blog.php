f187007227278ff7f1b37214d5b293d8a19c8e61
unknown <30684@fiecalunos.lan>
2019-11-28 15:00:45 -0300

    reset all

--- blog.php
+++ blog.php
@@ -1,12 +1,8 @@
-<?php include_once('./headFooter/head.php')  ?>
-<link rel="stylesheet" href="blogStyle.css">
+
+
+<?php include_once('headFooter/head.php') ?> 
 
 
 
-<!-- 
-https://i.ibb.co/Ms5Cj5Q/Image-URL-1.jpg
-https://i.ibb.co/grCvMY2/Image-URL.jpg
-https://i.ibb.co/r4F6rtk/Image-URL-2.jpg -->
 
-
-<?php include_once('./headFooter/footer.php')  ?>
+<?php include_once('headFooter/footer.php') ?> 
--- headFooter/head.php
+++ headFooter/head.php
@@ -1,53 +1,9 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Title</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-
-  <nav class="navbar navbar-dark bg-dark">
-    <div class="row mr-auto">
-      <div class="col col-md-2 align-items-end"><i class="fa fa-facebook" aria-hidden="true"></i></div>
-      <div class="col col-md-2 align-items-end"><i class="fa fa-instagram" aria-hidden="true"></i></div>
-      <div class="col col-md-2 align-items-end"><i class="fa fa-twitter" aria-hidden="true"></i></div>
-      <div class="col col-md-2 align-items-end"><i class="fa fa-youtube" aria-hidden="true"></i></div>
-    </div>
-</nav>
-
-
-  <nav class="navbar navbar-expand-lg navbar-dark bg-warning container">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/cgophp/index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/cgophp/blog.php">Blog</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
