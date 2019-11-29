d92f20075c759bea0e238de405bde075514ddaf9
unknown <30684@fiecalunos.lan>
2019-11-29 15:28:57 -0300

    saa

--- blog.php
+++ blog.php
@@ -1,3 +1,19 @@
+<style>
+.my-nav{
+position: absolute;
+z-index: 10;
+width: 100%;
+}
+
+/* body {    
+  background: url(https://i.ibb.co/r3kZnk6/asd.png) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+} */
+</style>
+
 <?php include_once('./headFooter/head.php') ?>
 
 <?php 
@@ -11,45 +27,44 @@ class Portfolio{
 }
 
 $portfolios = array(
-  new Portfolio('https://www.google.com/gmail/about/static/images/logo-gmail.png?cache=3218fbf','anota isso aqui perez'),
-  new Portfolio('https://www.google.com/gmail/about/static/images/logo-gmail.png?cache=3218fbf','anota isso aqui perez'),
-  new Portfolio('https://www.google.com/gmail/about/static/images/logo-gmail.png?cache=3218fbf','anota isso aqui perez'),
-  new Portfolio('https://www.google.com/gmail/about/static/images/logo-gmail.png?cache=3218fbf','anota isso aqui perez'),
+  new Portfolio('https://www.google.com/gmail/about/static/images/logo-gmail.png?cache=3218fbf','1'),
+  new Portfolio('https://www.google.com/gmail/about/static/images/logo-gmail.png?cache=3218fbf','2'),
+  new Portfolio('https://www.google.com/gmail/about/static/images/logo-gmail.png?cache=3218fbf','3'),
+  new Portfolio('https://www.google.com/gmail/about/static/images/logo-gmail.png?cache=3218fbf','4'),
 );
 ?>
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-  <a class="navbar-brand" href="#"><img src="https://i.ibb.co/kBrnH99/Logo-TCC.jpg"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
-    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav ml-auto ">
-      <li class="nav-item active">
-        <a class="nav-link h3" href="/cgophp/">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link h3" href="/cgophp/blog.php">Blog</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link h3" href="/cgophp/blog.php">Login</a>
-      </li>
-    </ul>
-  </div>
-</nav>
 
-<div class="row mx-auto">
-  <?php foreach ($portfolios as $cartao){ ?>
-  <div class="card col-sm-12 col-md-4 mx-4 mb-4">
-    <img class="card-img-top" src="<?=$cartao->imagem?>">
-    <div class="card-body">
-      <h3 class="card-title"><?=$cartao->descricao?></h3>
-    </div>
+<nav class=" my-nav navbar navbar-light navbar-expand-lg">
+      <div class="container">
+      <a class="navbar-brand" href="portfolio.php"><img src="https://i.ibb.co/WpDF2qy/Logo-TCC.png" height="100px" width="100px" alt="Logo da Empresa."></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link h4" href="#">Inicio <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link h4" href="#">Aplicativo</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link h4" href="#">Sobre o Projeto</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link h4" href="#">Suporte</a>
+          </li>
+        </ul>
+      </div>
   </div>
-  <?php } ?>
+    </nav>
+
+
+
+
+
 </div>
-
 
 <?php include_once('./headFooter/footer.php') ?>
