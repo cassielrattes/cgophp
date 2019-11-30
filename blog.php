0ae21b255444401fc82e6a51d957a5807d37f3fc
unknown <30684@fiecalunos.lan>
2019-11-29 22:47:56 -0300

    blog1

--- blog.php
+++ blog.php
@@ -6,6 +6,7 @@ class Portfolio{
     $this->titulo = $titulo;
     $this->imagem = $imagem;
     $this->descricao = $descricao;
+    // $this->modal = $modal;
   }
 }
 
@@ -16,17 +17,15 @@ $portfolios = array(
   /* Titulo */'Tipos de navegação no React Native',
   /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
   /* Descrição */
-  'Existem diversos tipos de navegação que podem ser usadas em um aplicativo com React Native.
-  Cada um desses tipos possui um objetivo e um tipo de implementação diferentes. Hoje, vamos entender quando, e como, utilizar cada um deles.'
+  ''
   ),
 
   new Portfolio
   (
-  /* Titulo */'Paginação com React Router Dom',
+  /* Titulo */'Paginação com React Router Dom32',
   /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
   /* Descrição */
-  'A paginação em uma página pode até parecer redundante, mas ela é um "órgão vital" para seu website, principalmente quando se trata de listas com enormes quantidades de informação, além de ser extremamente útil para acesso momentâneo, tal qual é a importância de guardar através do link as informações para chegar na mesma posteriormente.
-  Quando utilizamos um backend que trás o HTML processado para nós, é bem tranquilo trabalhar com elas, todavia tratando-se de SPA isso se torna um pouco chato. A parte "chata" em si não é definir a paginação, mas integrar ela ao browser de uma maneira prática e eficaz.'
+  ''
   ),  
 
   new Portfolio
@@ -34,7 +33,7 @@ $portfolios = array(
   /* Titulo */'Serverless: Quando utilizar e aplicações com NodeJS',
   /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
   /* Descrição */
-  'Ultimamente o termo serverless entrou em ascensão e muito se comenta sobre aplicações utilizarem essa arquitetura como forma de se ganhar performance evitando a configuração de servidores complexos através de containers ou até de arquiteturas mais tradicionais.'
+  ''
   ),  
 
   new Portfolio
@@ -42,101 +41,194 @@ $portfolios = array(
   /* Titulo */'Atualizando aplicações React Native para a última versão',
   /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
   /* Descrição */
-  'Se você desenvolve aplicativos usando React Native, deve ter reparado que ele está em constante evolução, uma versão nova é lançada quase mensalmente. A cada versão são feitas melhoras no Framework, sejam elas em performance, segurança, suporte a novas funcionalidades, etc...
-
-  A atualização de uma aplicação feita em React Native para a última versão é um processo não tão simples, mas o Facebook, juntamente com a Comunidade, criaram uma lib para facilitar esse processo, a rn-diff-purge.
-  
-  Essa lib identifica a versão do React Native da sua aplicação e, baseado nela, realiza as mudanças para o React Native ficar na última versão disponível.'
+  ''
   ),
-
+  new Portfolio
+  (
+  /* Titulo */'Atualizando aplicações React Native para a última versão',
+  /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
+  /* Descrição */
+  ''
+  ),
+  new Portfolio
+  (
+  /* Titulo */'Atualizando aplicações React Native para a última versão',
+  /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
+  /* Descrição */
+  ''
+  ),  new Portfolio
+  (
+  /* Titulo */'Atualizando aplicações React Native para a última versão',
+  /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
+  /* Descrição */
+  ''
+  ),  new Portfolio
+  (
+  /* Titulo */'Atualizando aplicações React Native para a última versão',
+  /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
+  /* Descrição */
+  ''
+  ),  new Portfolio
+  (
+  /* Titulo */'Atualizando aplicações React Native para a última versão',
+  /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
+  /* Descrição */
+  ''
+  ),  new Portfolio
+  (
+  /* Titulo */'Atualizando aplicações React Native para a última versão',
+  /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
+  /* Descrição */
+  ''
+  ),  new Portfolio
+  (
+  /* Titulo */'Atualizando aplicações React Native para a última versão',
+  /* Imagem */'https://www.guiadaengenharia.com/wp-content/uploads/2019/07/or%C3%A7amento-bdi.jpg',
+  /* Descrição */
+  ''
+  ),
 );
+
+$principal = $portfolios[0];
+array_splice($portfolios, 0,1);
 ?>
 
 <?php include_once('./headFooter/head.php') ?>
 
-<div class="container">
+<nav class=" my-nav navbar navbar-light navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand" href="portfolio.php"><img class="rounded-circle img-fluid" src="https://i.ibb.co/f04HKqX/Logo-Cassiel-TCC.png" height="100px"
+          width="100px" alt="Logo da Empresa."></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link h4" href="#">Inicio <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link h4" href="#">Aplicativo</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link h4" href="#">Sobre o Projeto</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link h4" href="#">Suporte</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
+<div class="jumbotron jumbotron-fluid" style="background-color:rgb(88,85,112)">
+  <div class="container">
+    <h1 class="display-4 text-center text-white">Blog do CGO</h1>
+  </div>
+</div>
+
+
+<div class="ml-5 mr-5">
   <div class="row">
-    <!-- Blog Entries Column -->
     <div class="col-md-8">
       <h1 class="my-4">Ultimas Noticias /
-        <small>Blog CGO</small>
+        <small>Edificações</small>
       </h1>
-      <!-- Blog Post -->
-      <?php foreach($portfolios as $cartao){ ?>
-      <div class="card mb-4">
-        <img class="card-img-top" src="<?=$cartao->imagem?>" alt="Card image cap">
-        <div class="card-body">
-          <h2 class="card-title text-center"><?=$cartao->titulo?></h2>
-          <p class="card-text text-center"><?=$cartao->descricao?></p>
-        </div>
-      </div>
-      <?php } ?>
-    </div>
+      <div class="row">
 
-    <!-- Sidebar Widgets Column -->
-    <div class="col-md-4">
-      <!-- Search Widget -->
-      <div class="card my-4">
-        <h5 class="card-header">Buscar</h5>
-        <div class="card-body">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Buscando...">
-            <span class="input-group-btn">
-              <button class="btn btn-secondary" type="button">Go!</button>
-            </span>
+        <div class="col-md-12 card mb-4">
+          <img class="card-img-top" src="<?=$principal->imagem?>" alt="Card image cap">
+          <div class="card-body">
+            <h2 class="card-title text-center"><?=$principal->titulo?></h2>
+            <p class="card-text text-center"><?=$principal->descricao?></p>
+            <div class="text-right" >
+                <a href="<?=$principal->imagem?>" class="btn btn-danger" id="btn" style=""><h6>Saiba mais</h6></a>
+              </div>
           </div>
         </div>
-      </div>
-
-      <!-- Categories Widget -->
-      <div class="card my-4">
-        <h5 class="card-header">Categories</h5>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-lg-6">
-              <ul class="list-unstyled mb-0">
-                <li>
-                  <a href="#">Web Design</a>
-                </li>
-                <li>
-                  <a href="#">HTML</a>
-                </li>
-                <li>
-                  <a href="#">Freebies</a>
-                </li>
-              </ul>
+        <?php foreach($portfolios as $cartao){ ?>
+        <div class="col-md-4 card mb-4 ml-0">
+          <div class="subcard">
+            <img class="card-img-top" src="<?=$cartao->imagem?>" alt="Card image cap">
+            <div class="card-body">
+              <h3 class="card-title"><?=$cartao->titulo?></h3>
+              <!-- <p class="card-text text-center"><?=$cartao->descricao?></p> -->
+              <div class="text-right" >
+                <a href="<?=$cartao->imagem?>" class="btn btn-danger" id="btn" style=""><h6>Saiba mais</h6></a>
+              </div>
             </div>
-            <div class="col-lg-6">
-              <ul class="list-unstyled mb-0">
-                <li>
-                  <a href="#">JavaScript</a>
-                </li>
-                <li>
-                  <a href="#">CSS</a>
-                </li>
-                <li>
-                  <a href="#">Tutorials</a>
-                </li>
-              </ul>
+          </div>
+
+        </div>
+        <?php } ?>
+      </div>
+      
+    </div>
+
+    <div id="author">
+        <div class="card my-4">
+          <h5 class="card-header">Buscar</h5>
+          <div class="card-body">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Buscando...">
+              <span class="input-group-btn">
+                <button class="btn btn-secondary" type="button">Go!</button>
+              </span>
             </div>
           </div>
         </div>
-      </div>
 
-      <!-- Side Widget -->
-      <div class="card my-4">
-        <h5 class="card-header">Autor</h5>
-        <img class="rounded-circle img-fluid mx-auto mt-3" style="height:150px; width:150px" src="https://i.ibb.co/ZJCywsk/Image-URL.jpg">
-        <div class="card-body">
-          <h5 class="text-center">Cassiel R. Cortez</h5>
+
+        <!-- <div class="card my-4">
+          <h5 class="card-header">Categories</h5>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-lg-6">
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="#">Web Design</a>
+                  </li>
+                  <li>
+                    <a href="#">HTML</a>
+                  </li>
+                  <li>
+                    <a href="#">Freebies</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="#">JavaScript</a>
+                  </li>
+                  <li>
+                    <a href="#">CSS</a>
+                  </li>
+                  <li>
+                    <a href="#">Tutorials</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div> -->
+
+        <div class="card my-4">
+          <h5 class="card-header">Autor</h5>
+          <img class="rounded-circle img-fluid mx-auto mt-3" style="height:150px; width:150px"
+            src="https://i.ibb.co/ZJCywsk/Image-URL.jpg">
+          <div class="card-body">
+            <h5 class="text-center">Cassiel R. Cortez</h5>
+          </div>
         </div>
+
+ 
+
+
+
       </div>
-
     </div>
-
-  </div>
-  <!-- /.row -->
 </div>
 
 
--- headFooter/head.php
+++ headFooter/head.php
@@ -1,44 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <style>
-  .my-nav {
-    z-index: 10;
-    width: 100%;
-      }
-    </style>
-  </head>
-  <body>
 
-  <nav class=" my-nav navbar navbar-light navbar-expand-lg">
-  <div class="container">
-    <a class="navbar-brand" href="portfolio.php"><img src="https://i.ibb.co/WpDF2qy/Logo-TCC.png" height="100px"
-        width="100px" alt="Logo da Empresa."></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link h4" href="#">Inicio <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link h4" href="#">Aplicativo</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link h4" href="#">Sobre o Projeto</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link h4" href="#">Suporte</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
