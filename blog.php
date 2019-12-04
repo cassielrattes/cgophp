6ccdc4a3da898007eaa3604c211172f235cc225b
unknown <30684@fiecalunos.lan>
2019-12-03 21:58:42 -0300

    arrumandinho

--- blog.php
+++ blog.php
@@ -81,24 +81,21 @@ array_splice($portfolios, 0,1);
 <nav class=" my-nav navbar navbar-light navbar-expand-lg">
   <div class="container">
     <a class="navbar-brand" href="portfolio.php"><img class="rounded-circle img-fluid"
-        src="https://i.ibb.co/f04HKqX/Logo-Cassiel-TCC.png" height="100px" width="100px" alt="Logo da Empresa."></a>
+        src="https://i.ibb.co/f04HKqX/Logo-Cassiel-TCC.png" alt="Logo da Empresa."></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
+    <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link h4" href="/cgophp/index.php">Inicio<span class="sr-only">(current)</span></a>
+          <a class="nav-link h4" href="index.php">Inicio<span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link h4" href="/cgophp/portfolio.php">Aplicativo</a>
+          <a class="nav-link h4" href="portfolio.php">Aplicativo</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link h4" href="/cgophp/blog.php">Sobre o Projeto</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link h4" href="/cgophp/portfolio.php">Suporte</a>
+          <a class="nav-link h4" href="blog.php">Blog</a>
         </li>
       </ul>
     </div>
--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 <nav class=" my-nav navbar navbar-light navbar-expand-lg">
   <div class="container">
     <a class="navbar-brand" href="portfolio.php"><img class="rounded-circle img-fluid"
-        src="https://i.ibb.co/f04HKqX/Logo-Cassiel-TCC.png" height="100px" width="100px" alt="Logo da Empresa."></a>
+        src="https://i.ibb.co/f04HKqX/Logo-Cassiel-TCC.png" alt="Logo da Empresa."></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
+    <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
           <a class="nav-link h4" href="/cgophp/index.php">Inicio<span class="sr-only">(current)</span></a>
         </li>
@@ -20,15 +20,18 @@
           <a class="nav-link h4" href="/cgophp/portfolio.php">Aplicativo</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link h4" href="/cgophp/blog.php">Sobre o Projeto</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link h4" href="/cgophp/portfolio.php">Suporte</a>
+          <a class="nav-link h4" href="/cgophp/blog.php">Blog</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
+
+<div class="jumbotron jumbotron-fluid" style="background-color:rgb(88,85,112)">
+  <div class="container">
+    <h1 class="display-4 text-center text-white">Home</h1>
+  </div>
+</div>
 
 
 <?php include_once('./headFooter/footer.php') ?>
